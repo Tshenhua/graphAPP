e7ef9d5c4d00b541987afd58eeb28c5e2ab4d5b7
MacTai <591390744@qq.com>
2020-07-27 23:40:57 +0800

    Initial Commit

---
+++ graphAPP/graphAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  graphAPPApp.swift
+//  graphAPP
+//
+//  Created by Medcon on 2020/7/27.
+//
+
+import SwiftUI
+
+@main
+struct graphAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
